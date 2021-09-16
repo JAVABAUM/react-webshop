12b1e7b7845d6aaefad0cebeb38c2d644230088a
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-09-16 08:22:58 +0200

    topbar added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Topbar from "./components/topbar/topbar";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <a>
           JAVABAUM
         </a>
+        <Topbar/>
       </header>
     </div>
   );
