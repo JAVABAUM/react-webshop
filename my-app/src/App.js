72da0ac471dce6e5fc94620a22ba39fa3100e5db
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-09-16 09:30:06 +0200

    shop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Topbar from "./components/topbar/topbar";
 import Footer from "./components/footer/footer";
+import Shop from "./components/shop/shop";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Topbar/>
-
+        <Shop/>
         <Footer/>
 
       </header>
