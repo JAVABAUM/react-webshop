6da7ae3019aff124e3618a4b7d4f84f7ca37769e
slaxeea <kappelers77@gmail.com>
2021-09-23 07:36:53 +0200

    start of the shop

--- my-app/src/components/shop/product.jsx
+++ my-app/src/components/shop/product.jsx
@@ -1,37 +1,16 @@
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { initializeApp } from "firebase/app";
-
 import React from "react";
 import './shop.css'
+import placeholder from './placeholder.jpg';
 
 export default function Product(props) {
     return (
         <>
             <div className="product">
                 <a href="/home" className="product-link">
-                    <h3>Title</h3>
-                    <img src={getImage()} alt="image" className="product-img" id="image" />
+                    <h3>{props.title}</h3>
+                    <img src={placeholder} alt="image" className="product-img"/>
                 </a>
             </div>
         </>
     );
-}
-
-function getImage() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyAvxvEtENC1DGh00_tIv6K0Wg8qZ1rMlb4",
-        authDomain: "webshop-f0863.firebaseapp.com",
-        projectId: "webshop-f0863",
-        storageBucket: "webshop-f0863.appspot.com",
-        messagingSenderId: "727095280293",
-        appId: "1:727095280293:web:718b2c3fffc99ec311c34c"
-    };
-
-    const app = initializeApp(firebaseConfig);
-
-    const storage = getStorage();
-    const path = ref(storage, '/placeholder.jpg');
-    getDownloadURL(path).then((url) => {
-        document.getElementById("image").setAttribute("src", url);
-    })
 }
