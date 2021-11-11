befd461213c8b73859c2e1e278c0d541d7ed376b
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-11-11 08:29:24 +0100

    Navbar update

--- my-app/src/components/topbar/topbar.jsx
+++ my-app/src/components/topbar/topbar.jsx
@@ -2,11 +2,16 @@ import "./topbar.css"
 import "../shop/cart"
 import React, { Component } from "react";
 import ecomCart from '@ecomplus/shopping-cart'
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, query, getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
 export default function Topbar() {
     return (
         <>
+            
             <div className="navbar">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
@@ -23,12 +28,16 @@ export default function Topbar() {
                             </ul>
                         </div>
 
+                        <div id="iconDiv">
+                            
+                            <div id="p1"></div>
+                        </div>
 
                         <div id="iconDiv">
                             <a href="/login">
                                 <div className="headerIcon">
                                     <img id="icon" src="/media/images/user.png"></img>
-                                    Login
+
                                 </div>
 
                             </a>
@@ -42,9 +51,60 @@ export default function Topbar() {
                                 </div>
                             </a>
                         </div>
+                        <div id="iconDiv" >
+                            <a onclick="firebaseLogout()" href="/login">
+                                <div className="headerIcon">
+                                    Logout
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </nav>
             </div>
         </>
     );
 }
+const firebaseConfig = {
+    apiKey: "AIzaSyAvxvEtENC1DGh00_tIv6K0Wg8qZ1rMlb4",
+    authDomain: "webshop-f0863.firebaseapp.com",
+    projectId: "webshop-f0863",
+    storageBucket: "webshop-f0863.appspot.com",
+    messagingSenderId: "727095280293",
+    appId: "1:727095280293:web:718b2c3fffc99ec311c34c"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+const auth = getAuth();
+const user = auth.currentUser;
+function getmail(){
+    if (user !== null) {
+        const displayName = user.displayName;
+        const email = user.email;
+        const photoURL = user.photoURL;
+        const emailVerified = user.emailVerified;
+
+        const uid = user.uid;
+        console.log(email);
+        document.getElementById("p1").innerHTML = getemailbf(email);
+
+    }
+};
+
+function getemailbf(email) {
+    return email.substring(0, email.indexOf("@"));
+}
+
+function firebaseLogout() {
+    auth.signOut().then(function () {
+        console.log("Logged out");
+        document.getElementById("p1").innerHTML = "";
+    }).catch(function (error) {
+        console.log("Error logging out");
+    });
+}
+onAuthStateChanged(auth, (user) => {
+    getmail();
+});
+
